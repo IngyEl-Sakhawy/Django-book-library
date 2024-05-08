c044268eb9c8247358b4b135ce26ae99908d7035
ingy <ingy.mahmoud.elsakhawy@gmail.com>
2024-05-09 01:23:31 +0300

    Lab2

--- myproject/books/models.py
+++ myproject/books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from django.core import validators
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(
+        validators=[
+            validators.MinLengthValidator(10),
+            validators.MaxLengthValidator(500),
+        ]
+    )
+    
+    
+
+    def __str__(self):
+        return self.title
 
